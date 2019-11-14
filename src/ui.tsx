542f14ab8dc310ab68ccc981197b016e2eef192e
Scott Takahashi <scott.r.takahashi@gmail.com>
2019-11-13 16:37:59 -0800

    Get Studio working

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,40 +1,53 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import {
+    Heading,
+    ThemeProvider,
+    themeRefresh,
+    GlobalStyles,
+} from '@patreon/studio'
 
 class App extends React.Component {
-  textbox: HTMLInputElement;
+    textbox: HTMLInputElement
 
-  countRef = (element: HTMLInputElement) => {
-    if (element) element.value = "5";
-    this.textbox = element;
-  };
+    countRef = (element: HTMLInputElement) => {
+        if (element) element.value = '5'
+        this.textbox = element
+    }
 
-  onCreate = () => {
-    const count = parseInt(this.textbox.value, 10);
-    parent.postMessage(
-      { pluginMessage: { type: "create-rectangles", count } },
-      "*"
-    );
-  };
+    onCreate = () => {
+        const count = parseInt(this.textbox.value, 10)
+        parent.postMessage(
+            { pluginMessage: { type: 'create-rectangles', count } },
+            '*',
+        )
+    }
 
-  onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
-  };
+    onCancel = () => {
+        parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Rectangle Creator</h2>
-        <p>
-          Count: <input ref={this.countRef} />
-        </p>
-        <button id="create" onClick={this.onCreate}>
-          Create
-        </button>
-        <button onClick={this.onCancel}>Cancel</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider theme={themeRefresh}>
+                <>
+                    <GlobalStyles.GlobalBaseStyles />
+                    <GlobalStyles.GlobalNormalizeStyles />
+                    <div>
+                        <Heading>Rectangle Creator</Heading>
+                        <p>
+                            Count: <input ref={this.countRef} />
+                        </p>
+                        <button id="create" onClick={this.onCreate}>
+                            Create
+                        </button>
+                        <button onClick={this.onCancel}>Cancel</button>
+                    </div>
+                </>
+            </ThemeProvider>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById("react-page"));
+ReactDOM.render(<App />, document.getElementById('react-page'))
